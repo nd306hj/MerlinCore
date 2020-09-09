9de39eb99be1339685c7fcd41fda24dbd620b847
Kellei@KELLEI-SFC <tukanlh@gmail.com>
2020-09-09 15:06:38 +0200

    awesome commit - added background texture support

---
+++ Engine/Game/BackgroundTile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Merlin.Game
+{
+    class BackgroundTile
+    {
+        public Vector2 Position { get; private set; }
+        public int TileCode { get; private set; }
+        public BackgroundTile(int posX, int posY, int code)
+        {
+            Position = new Vector2(posX, posY);
+            TileCode = code;
+        }
+    }
+}
--- Engine/Game/Map.cs
+++ Engine/Game/Map.cs
@@ -2,12 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using TiledSharp;
 
 namespace Merlin.Game
 {
-    public class Map
+    public class Map : IDisposable
     {
         public int Width { get; private set; }
         public int Height { get; private set; }
@@ -15,9 +16,12 @@ namespace Merlin.Game
         public int TileHeight { get; private set; }
 
         private int[,] walls;
-        private int[,] background;
+        //private int[,] background;
 
-        private Texture2D[] textures;
+        private BackgroundTile[,] backgroundTiles;
+
+        private Texture2D backgroundTexture;
+        private bool disposedValue;
 
         public Map(string mapResource)
         {
@@ -51,14 +55,17 @@ namespace Merlin.Game
 
         private void LoadBackground(TmxMap tiledMap)
         {
-            background = new int[tiledMap.Width, tiledMap.Height];
+
+            backgroundTexture = Raylib.LoadTexture(tiledMap.ImageLayers[0].Image.Source);
+
+            backgroundTiles = new BackgroundTile[tiledMap.Width, tiledMap.Height];
 
             for (int x = 0; x < tiledMap.Width; x++)
             {
                 for (int y = 0; y < tiledMap.Height; y++)
                 {
-                    walls[x, y] = tiledMap.Layers["background"].Tiles.Single(tile => tile.X == x && tile.Y == y).Gid;
-
+                    backgroundTiles[x, y] = new BackgroundTile(x * tiledMap.TileWidth, y * tiledMap.TileHeight,
+                        tiledMap.Layers["background"].Tiles.Single(tile => tile.X == x && tile.Y == y).Gid);
                 }
             }
         }
@@ -73,6 +80,34 @@ namespace Merlin.Game
             walls[x, y] = wall ? 1 : 0;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
 
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                Raylib.UnloadTexture(backgroundTexture);
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Map()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
