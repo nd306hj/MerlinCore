3c2c9a7c0d38bc797b1d5e31a52be62de9bb7c1b
nd306nj <nikoleta.dimunova@student.tuke.sk>
2023-01-03 20:18:43 +0100

    initial commit

---
+++ Merlin2dTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Merlin2dTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
